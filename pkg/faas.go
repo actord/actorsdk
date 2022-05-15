6b02f4cac9b909d2b1554e3946d3b42ee584d3fe
Ruslan Cheshko <chex@soul.li>
2022-05-15 17:05:17 +0300

    remove logging

--- pkg/faas.go
+++ pkg/faas.go
@@ -87,13 +87,9 @@ func (f *FaaS) Listen() error {
 	http.HandleFunc("/healthcheck", WithLogging(f.handleHealthcheck))
 
 	if f.endpoints != nil {
-		log.Println("has endpoints")
 		for endpoint, handlerFunc := range f.endpoints {
-			log.Println("add handler for endpoint:", endpoint)
 			http.HandleFunc("/"+endpoint, WithLogging(handlerFunc))
 		}
-	} else {
-		log.Println("endpoints is not provided")
 	}
 
 	log.Printf("Listen and serve at :%s, revision: %s", f.Port, f.revision)
